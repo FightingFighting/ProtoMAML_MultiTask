601d37b92354952b0f3ab3da8458db31564223c9
Mario Holubar <34423543+mario-holubar@users.noreply.github.com>
2021-05-25 23:40:23 +0200

    rewrite again
    faster method that uses more memory

--- models/protomaml.py
+++ models/protomaml.py
@@ -14,17 +14,6 @@ class ProtoMAML_framework(MAML_framework):
     def __init__(self, args, classifier):
         super(ProtoMAML_framework, self).__init__(args, classifier)
 
-    def generate_newModel_instance(self, x_support_set, y_support_set):
-
-        # generate all prototypes
-        c_prototypes = self.get_allPrototypes(x_support_set, y_support_set).detach() #(num_class, dim_encoder_hidden)
-
-        classifier_new = deepcopy(self.classifier_init)
-        classifier_new.fc_layer.weight.data = c_prototypes*2.0 #(num_class, dim_encoder_hidden)
-        classifier_new.fc_layer.bias.data = -1.0*c_prototypes.norm(dim=1)**2 #(num_class,)
-
-        return classifier_new
-
     def get_allPrototypes(self, x_support_set, y_support_set):
         """
          calculate the prototype for all class
@@ -52,14 +41,14 @@ class ProtoMAML_framework(MAML_framework):
 
     def train_protomaml(self, data_iter_train, criterion):
         
-        grad_sim = pd.DataFrame(columns = ["sim_k", "sim_init", "sim_both"])
+        grad_sim = pd.DataFrame()
         
         optimizer_meta = optim.SGD(self.classifier_init.encoder.parameters(), lr=self.args.lr_beta)
         
         for epoch in range(self.args.num_epoch) :
             # sample batch of tasks
             for indx_batch_tasks, data_batch_tasks in enumerate(data_iter_train):
-
+                
                 loss_batch_tasks = []
                 acc_batch_tasks = []
                 
@@ -71,63 +60,77 @@ class ProtoMAML_framework(MAML_framework):
                 
                 assert len(data_batch_tasks.values()) == 2 # gradient conflict code assumes two datasets are used
                 
-                # Compute gradients for random batch of each dataset
-                # This is not used to update the model, only as a reference measure
-                '''ref_batch = next(iter(data_iter_train))
-                first = True
-                for data_per_task in ref_batch.values():
-                    support_set, query_set = data_per_task['support'],  data_per_task['query']
-                    x_support_set, y_support_set = (support_set['input_ids'], support_set['attention_mask']), support_set['targets']
-                    self.classifier_episode = self.generate_newModel_instance(x_support_set, y_support_set)
-                    optimizer_task = optim.SGD(filter(lambda p: p.requires_grad, self.classifier_episode.parameters()), lr=self.args.lr_alpha)
-                    outer_grad, _, _, inner_grad = self.train_episode(criterion, data_per_task, optimizer_task)
-                    ref_grad_1 = [g.detach().flatten() for g in outer_grad]
-                    break'''
-
                 #for each task/episode
                 for t, data_per_task in enumerate(data_batch_tasks.values()):
-
+                    
+                    # STEP 1 sample data
                     support_set, query_set = data_per_task['support'],  data_per_task['query']
                     x_support_set, y_support_set = (support_set['input_ids'], support_set['attention_mask']), support_set['targets']
-
-                    # copy model
-                    self.classifier_episode = self.generate_newModel_instance(x_support_set, y_support_set)
-                    optimizer_task = optim.SGD(filter(lambda p: p.requires_grad, self.classifier_episode.parameters()), lr=self.args.lr_alpha)
-
-                    # train episode
-                    grads_query_k, loss_query, accuracy_query, encoded_query = self.train_episode(criterion, data_per_task, optimizer_task)
-
-                    # accumulate loss_query and acc
-                    loss_batch_tasks.append(loss_query)
-                    acc_batch_tasks.append(accuracy_query)
                     
-                    assert len(grads_query_k) == len(list(self.classifier_episode.named_parameters()))
+                    # STEP 2 duplicate model
+                    self.classifier_episode = deepcopy(self.classifier_init)
+                    for ind, param in enumerate(self.classifier_episode.encoder.parameters()):
+                        if param.grad is not None:
+                            print("new model has gradient!")
+                            param.grad = None
                     
+                    # STEP 3 calculate prototypes
+                    prototypes = self.get_allPrototypes(x_support_set, y_support_set) #(num_class, dim_encoder_hidden)
                     
+                    # STEP 4 initialize final layer using prototypes
+                    #classifier_episode.fc_layer.weight.data = c_prototypes.detach() * 2.0 #(num_class, dim_encoder_hidden)
+                    #classifier_episode.fc_layer.bias.data = -1.0 * c_prototypes.detach().norm(dim=1)**2 #(num_class,)
+                    fc_weight = prototypes.detach()*2.0 #(num_class, dim_encoder_hidden)
+                    fc_bias = -1.0*prototypes.detach().norm(dim=1)**2 #(num_class,)
                     
-                    # Calculate prototype initialization (with gradients)
-                    prototypes = self.get_allPrototypes(x_support_set, y_support_set)
+                    # STEP 5 train episode
+                    x_support_set, y_support_set = (support_set['input_ids'], support_set['attention_mask']), support_set['targets']
+                    optimizer_task = optim.SGD(self.classifier_episode.parameters(), lr=self.args.lr_alpha)
+                    for i in range(self.args.train_step_per_episode):
+                        #preds_support, _ = self.classifier_episode(*x_support_set) # TODO manual fc?
+                        encoded_support = self.classifier_episode.encoder(*x_support_set).pooler_output
+                        preds_support = encoded_support @ fc_weight.T + fc_bias
+                        loss_support = criterion(preds_support, y_support_set)
+                        optimizer_task.zero_grad()
+                        loss_support.backward()
+                        optimizer_task.step()
                     
-                    # Reintroduce prototypes to computation graph
+                    # STEP 6 reintroduce prototypes to computation graph
                     p_weight = prototypes*2.0 #(num_class, dim_encoder_hidden)
                     p_bias = -1.0*prototypes.norm(dim=1)**2 #(num_class,)
-                    #self.classifier_episode.fc_layer.weight.data = p_weight + (self.classifier_episode.fc_layer.weight.data - p_weight).detach()
-                    #self.classifier_episode.fc_layer.bias.data = p_bias + (self.classifier_episode.fc_layer.bias.data - p_bias).detach()
-                    #self.classifier_episode.fc_layer.weight.data = torch.nn.Parameter(p_weight, requires_grad=True)
-                    #self.classifier_episode.fc_layer.bias.data = torch.nn.Parameter(p_bias, requires_grad=True)
-                    #print(self.classifier_episode.fc_layer.weight.is_leaf) # TRUE
-                    
                     fc_weight = p_weight + (self.classifier_episode.fc_layer.weight.data - p_weight).detach()
                     fc_bias = p_bias + (self.classifier_episode.fc_layer.bias.data - p_bias).detach()
                     
-                    # Forward encoded query set through fc layer
-                    #preds_query = self.classifier_init.fc_layer(encoded_query)
-                    preds_query = encoded_query @ fc_weight.T + fc_bias
-                    
-                    # Calculate gradients for base model
-                    y_query_set = query_set['targets']
+                    # STEP 7 get gradients from query set
+                    x_query_set, y_query_set = (query_set['input_ids'],query_set['attention_mask']), query_set['targets']
+                    encoded_query = self.classifier_episode.encoder(*x_query_set).pooler_output
+                    preds_query = encoded_query @ fc_weight.T + fc_bias # Have to do it this way to keep gradients
                     loss_query = criterion(preds_query, y_query_set)
-                    grads_query_init = torch.autograd.grad(loss_query, self.classifier_init.encoder.parameters())
+                    accuracy_query = torch.sum(torch.argmax(preds_query,dim=-1) == y_query_set) / len(y_query_set)
+                    
+                    n_params = len(list(self.classifier_init.encoder.parameters()))
+                    params_k = self.classifier_episode.encoder.parameters()
+                    params_init = self.classifier_init.encoder.parameters()
+                    all_params = list(params_k) + list(params_init)
+                    grads_query_all = torch.autograd.grad(loss_query, all_params)
+                    grads_query_k = grads_query_all[:n_params]
+                    grads_query_init = grads_query_all[n_params:]
+                    
+                    assert len(grads_query_k) == len(list(self.classifier_init.encoder.named_parameters())) # sanity check
+                    assert len(grads_query_init) == len(list(self.classifier_init.encoder.named_parameters())) # sanity check
+                    
+                    # STEP 8 store in model
+                    for ind, param in enumerate(self.classifier_init.encoder.parameters()):
+                        grad_query = grads_query_k[ind] + grads_query_init[ind]
+                        #grad_query = grads_query_k[ind]
+                        if param.grad is None:
+                            param.grad = grad_query
+                        else:
+                            param.grad += grad_query
+                    
+                    # save loss_query and acc
+                    loss_batch_tasks.append(loss_query.detach().cpu().numpy())
+                    acc_batch_tasks.append(accuracy_query.detach().cpu().numpy())
                     
                     # Save gradients for conflict calculation
                     grads_k = []
@@ -140,30 +143,29 @@ class ProtoMAML_framework(MAML_framework):
                     task_grads_k.append(torch.cat(grads_k))
                     task_grads_init.append(torch.cat(grads_init))
                     task_grads_both.append(torch.cat(grads_both))
-                    
-                    # Store in model
-                    for ind, param in enumerate(self.classifier_init.encoder.parameters()):
-                        grad_query = grads_query_k[ind] + grads_query_init[ind]
-                        if param.grad is None:
-                            param = grad_query
-                        else:
-                            param += grad_query
                 
-                #update initial parameters
+                # Outer loop update
                 optimizer_meta.step()
                 optimizer_meta.zero_grad()
+                for param in self.classifier_init.encoder.parameters():
+                    #if param.requires_grad:
+                    #    param.data -= param.grad * self.args.lr_beta
+                    param.grad = None
                 
                 # Compute and save similarities
-                sim_k = torch.nn.functional.cosine_similarity(task_grads_k[0], task_grads_k[1], 0).item()
-                sim_init = torch.nn.functional.cosine_similarity(task_grads_init[0], task_grads_init[1], 0).item()
+                sims = {}
+                sims["loss"] = np.mean(loss_batch_tasks)
+                sims["sim_k"] = torch.nn.functional.cosine_similarity(task_grads_k[0], task_grads_k[1], 0).item()
+                sims["sim_init"] = torch.nn.functional.cosine_similarity(task_grads_init[0], task_grads_init[1], 0).item()
                 sim_both = torch.nn.functional.cosine_similarity(task_grads_both[0], task_grads_both[1], 0).item()
-                grad_sim.loc[len(grad_sim)] = [sim_k, sim_init, sim_both]
+                sims["sim_both"] = sim_both
+                grad_sim = grad_sim.append(sims, ignore_index=True)
                 grad_sim.to_csv("sim_" + emotion_1 + "_" + emotion_2 + ".csv")
                 
-                for t in [0, 1]:
+                '''for t in [0, 1]:
                     print(f"k_{t}: {np.linalg.norm(task_grads_k[t].cpu())}")
                     print(f"init_{t}: {np.linalg.norm(task_grads_init[t].cpu())}")
-                    print(f"both_{t}: {np.linalg.norm(task_grads_both[t].cpu())}")
+                    print(f"both_{t}: {np.linalg.norm(task_grads_both[t].cpu())}")'''
                 
                 # save checkpoint
                 if indx_batch_tasks % 10 == 0:
@@ -171,29 +173,4 @@ class ProtoMAML_framework(MAML_framework):
                     os.makedirs(model_dir, exist_ok=True)
                     torch.save({"args": self.args, "epoch": epoch, "step": indx_batch_tasks, "state_dict": self.state_dict}, os.path.join(model_dir, emotion_1 + "_" + emotion_2 + ".pt"))
 
-                print("indx_batch_tasks:", indx_batch_tasks," loss:", np.mean(loss_batch_tasks), " acc:", np.mean(acc_batch_tasks), " similarity:", sim_both)
-
-    def train_episode(self, criterion, data_per_task, optimizer_task):
-
-        support_set, query_set = data_per_task['support'],  data_per_task['query']
-        x_support_set, y_support_set = (support_set['input_ids'], support_set['attention_mask']), support_set['targets']
-        x_query_set, y_query_set = (query_set['input_ids'],query_set['attention_mask']), query_set['targets']
-        
-        for i in range(self.args.train_step_per_episode):
-            preds_support, _ = self.classifier_episode(*x_support_set)
-            loss_support = criterion(preds_support, y_support_set)
-
-            optimizer_task.zero_grad()
-            loss_support.backward()
-            
-            optimizer_task.step()
-
-        #generate loss for query set
-        preds_query, encoded_query = self.classifier_episode(*x_query_set)
-        loss_query = criterion(preds_query, y_query_set)
-        accuracy_query = torch.sum(torch.argmax(preds_query,dim=-1) == y_query_set) / len(y_query_set)
-
-        optimizer_task.zero_grad()
-        grads_query = torch.autograd.grad(loss_query, filter(lambda p: p.requires_grad, self.classifier_episode.parameters()))
-
-        return grads_query, loss_query.cpu().detach().numpy(), accuracy_query.cpu().detach().numpy(), encoded_query
+                print("indx_batch_tasks:", indx_batch_tasks, " loss:", np.mean(loss_batch_tasks), " acc:", np.mean(acc_batch_tasks), " similarity:", sim_both)
